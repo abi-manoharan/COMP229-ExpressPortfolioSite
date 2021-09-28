d05dc520b30b9fb938910c91a02c1894e1ba37a9
abi-manoharan <abimanoharan@outlook.com>
2021-09-27 21:58:12 -0400

    updates site with background, navbar, footer

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', page: 'Home'});
 });
+
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Express', page: 'Home'});
+});
+
+
+router.get('/AboutMe', function(req, res, next) {
+  res.render('index', { title: 'Express', page: 'About'});
+});
+
+router.get('/Services', function(req, res, next) {
+  res.render('index', { title: 'Express', page: 'Services'});
+});
+
+router.get('/Projects', function(req, res, next) {
+  res.render('index', { title: 'Express', page: 'Projects'});
+});
+
+router.get('/Contact', function(req, res, next) {
+  res.render('index', { title: 'Express', page: 'Contact'});
+});
+
 
 module.exports = router;
