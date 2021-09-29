17ea548dd44ac1554c0f8b54005995c706a397f7
abi-manoharan <abimanoharan@outlook.com>
2021-09-28 22:39:04 -0400

    Added content to home&about me. Restructured files

--- routes/index.js
+++ routes/index.js
@@ -3,28 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', page: 'Home'});
+  res.render('index', { page: 'Home'});
 });
 
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Express', page: 'Home'});
+  res.render('index', {  page: 'Home'});
 });
 
 
 router.get('/AboutMe', function(req, res, next) {
-  res.render('index', { title: 'Express', page: 'About'});
+  res.render('index', {  page: 'About Me'});
 });
 
 router.get('/Services', function(req, res, next) {
-  res.render('index', { title: 'Express', page: 'Services'});
+  res.render('index', {  page: 'Services'});
 });
 
 router.get('/Projects', function(req, res, next) {
-  res.render('index', { title: 'Express', page: 'Projects'});
+  res.render('index', {  page: 'Projects'});
 });
 
 router.get('/Contact', function(req, res, next) {
-  res.render('index', { title: 'Express', page: 'Contact'});
+  res.render('index', {  page: 'Contact'});
 });
 
 
