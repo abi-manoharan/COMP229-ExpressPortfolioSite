ae761d2c655b2ccf21e978869b4298366fc672ab
abi-manoharan <abimanoharan@outlook.com>
2021-09-29 21:07:57 -0400

    updated package.js for heroku

--- routes/index.js
+++ routes/index.js
@@ -5,24 +5,26 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { page: 'Home'});
 });
-
 router.get('/home', function(req, res, next) {
   res.render('index', {  page: 'Home'});
 });
 
-
+/* GET about me page. */
 router.get('/AboutMe', function(req, res, next) {
   res.render('index', {  page: 'About Me'});
 });
 
+/* GET services page. */
 router.get('/Services', function(req, res, next) {
   res.render('index', {  page: 'Services'});
 });
 
+/* GET projects page. */
 router.get('/Projects', function(req, res, next) {
   res.render('index', {  page: 'Projects'});
 });
 
+/* GET contact page. */
 router.get('/Contact', function(req, res, next) {
   res.render('contact', {  page: 'Contact'});
 });
